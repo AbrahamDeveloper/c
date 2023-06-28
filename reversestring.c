4286b0a3b39616aee3cdd6ed214af21aa249d3c6
Abraham Gomez <138025920+AbrahamDeveloper@users.noreply.github.com>
2023-06-28 12:50:28 -0400

    Create reversestring.c

---
+++ reversestring.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char* str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        // Swap characters
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        // Move pointers
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  // Remove trailing newline character
+
+    reverseString(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
